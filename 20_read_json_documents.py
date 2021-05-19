e9d58ed6c5c9070ecd5e88b784a1b8bac690974f
ong-ekken <ong.yuh.chian@gmail.com>
2021-05-19 16:10:10 +0900

    Add knocks 20-29.

---
+++ 20_read_json_documents.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+
+
+Find the frequency of a string in the first column, and sort the 
+strings by descending order of their frequencies. 
+
+Confirm the result by using cut, uniq, and sort commands.
+"""
